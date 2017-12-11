44cd7603b1670371f9a158ea948a62243a05f81b
PoojaGupta <pooja_gupta@msn.com>
2017-12-11 06:37:29 -0600

    refactored

--- src/modules/month/calendar-month-view-header.component.ts
+++ src/modules/month/calendar-month-view-header.component.ts
@@ -3,29 +3,8 @@ import { WeekDay } from 'calendar-utils';
 
 @Component({
   selector: 'mwl-calendar-month-view-header',
-  template: `
-    <ng-template
-      #defaultTemplate
-      let-days="days"
-      let-locale="locale">
-      <div class="cal-cell-row cal-header">
-        <div
-          class="cal-cell"
-          *ngFor="let day of days"
-          [class.cal-past]="day.isPast"
-          [class.cal-today]="day.isToday"
-          [class.cal-future]="day.isFuture"
-          [class.cal-weekend]="day.isWeekend"
-          [ngClass]="day.cssClass">
-          {{ day.date | calendarDate:'monthViewColumnHeader':locale }}
-        </div>
-      </div>
-    </ng-template>
-    <ng-template
-      [ngTemplateOutlet]="customTemplate || defaultTemplate"
-      [ngTemplateOutletContext]="{days: days, locale: locale}">
-    </ng-template>
-  `
+  templateUrl:'./calendar-month-view-header.component.html'
+
 })
 export class CalendarMonthViewHeaderComponent {
   @Input() days: WeekDay[];
--- src/modules/month/calendar-open-day-events.component.ts
+++ src/modules/month/calendar-open-day-events.component.ts
@@ -10,40 +10,7 @@ import { CalendarEvent } from 'calendar-utils';
 
 @Component({
   selector: 'mwl-calendar-open-day-events',
-  template: `
-    <ng-template
-      #defaultTemplate
-      let-events="events"
-      let-eventClicked="eventClicked">
-      <div
-        *ngFor="let event of events"
-        [ngClass]="event?.cssClass"
-        mwlDraggable
-        [dropData]="{event: event}"
-        [dragAxis]="{x: event.draggable, y: event.draggable}">
-        <span
-          class="cal-event"
-          [style.backgroundColor]="event.color.primary">
-        </span>
-        <mwl-calendar-event-title
-          [event]="event"
-          [customTemplate]="eventTitleTemplate"
-          view="month"
-          (mwlClick)="eventClicked.emit({event: event})">
-        </mwl-calendar-event-title>
-        <mwl-calendar-event-actions [event]="event"></mwl-calendar-event-actions>
-      </div>
-    </ng-template>
-    <div class="cal-open-day-events" [@collapse] *ngIf="isOpen">
-      <ng-template
-        [ngTemplateOutlet]="customTemplate || defaultTemplate"
-        [ngTemplateOutletContext]="{
-          events: events,
-          eventClicked: eventClicked
-        }">
-      </ng-template>
-    </div>
-  `,
+  templateUrl:'./calendar-open-day-events.component.html',
   animations: [
     trigger('collapse', [
       transition('void => *', [
