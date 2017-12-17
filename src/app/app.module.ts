164349fcdd321afc7393157aa64084b00976a7a8
PoojaGupta <pooja_gupta@msn.com>
2017-12-17 11:04:44 -0600

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,86 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
-//import '../angular-calendar.scss';
 import { AppComponent } from './app.component';
 import { EventDialogComponent } from './event-dialog/event-dialog.component';
-// import {
-//   MatAutocompleteModule,
-//   MatButtonModule,
-//   MatButtonToggleModule,
-//   MatCardModule,
-//   MatCheckboxModule,
-//   MatChipsModule,
-//   MatDatepickerModule,
-//   MatDialogModule,
-//   MatExpansionModule,
-//   MatGridListModule,
-//   MatIconModule,
-//   MatInputModule,
-//   MatListModule,
-//   MatMenuModule,
-//   MatNativeDateModule,
-//   MatPaginatorModule,
-//   MatProgressBarModule,
-//   MatProgressSpinnerModule,
-//   MatRadioModule,
-//   MatRippleModule,
-//   MatSelectModule,
-//   MatSidenavModule,
-//   MatSliderModule,
-//   MatSlideToggleModule,
-//   MatSnackBarModule,
-//   MatSortModule,
-//   MatTableModule,
-//   MatTabsModule,
-//   MatToolbarModule,
-//   MatTooltipModule,
-//   MatStepperModule,
-// } from '@angular/material';
-
-// import {CdkTableModule} from '@angular/cdk/table';
-//import { CalendarModule } from 'angular-calendar';
 
 import {CalendarModule} from '../modules/calendar.module';
 import {NGDemoMaterialModule} from '../material-module';
-
-// @NgModule({
-//   exports: [
-//     CdkTableModule,
-//     MatAutocompleteModule,
-//     MatButtonModule,
-//     MatButtonToggleModule,
-//     MatCardModule,
-//     MatCheckboxModule,
-//     MatChipsModule,
-//     MatStepperModule,
-//     MatDatepickerModule,
-//     MatDialogModule,
-//     MatExpansionModule,
-//     MatGridListModule,
-//     MatIconModule,
-//     MatInputModule,
-//     MatListModule,
-//     MatMenuModule,
-//     MatNativeDateModule,
-//     MatPaginatorModule,
-//     MatProgressBarModule,
-//     MatProgressSpinnerModule,
-//     MatRadioModule,
-//     MatRippleModule,
-//     MatSelectModule,
-//     MatSidenavModule,
-//     MatSliderModule,
-//     MatSlideToggleModule,
-//     MatSnackBarModule,
-//     MatSortModule,
-//     MatTableModule,
-//     MatTabsModule,
-//     MatToolbarModule,
-//     MatTooltipModule,
-//   ]
-// })
-// export class NGDemoMaterialModule {}
 
 @NgModule({
   declarations: [
