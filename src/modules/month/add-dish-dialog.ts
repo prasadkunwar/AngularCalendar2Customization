0cd15f89edfd8aa058724483506494912bc707ec
PKunwar <prasad_kunwar@hotmail.com>
2017-12-31 13:35:32 -0600

    update calendar meal view based on meal change done by add dish dialog

--- src/modules/month/add-dish-dialog.ts
+++ src/modules/month/add-dish-dialog.ts
@@ -4,52 +4,54 @@ import { MonthViewDay } from 'calendar-utils';
 import { FormControl } from '@angular/forms';
 import  'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
-
-export interface Recipe {
-  name: string;
-  time: number;
-}
-
+import {Recipe} from  './Recipe';
 
 @Component({
   selector: 'add-dish-dialog',
   templateUrl: 'add-dish-dialog.html',
 })
 export class AddDishDialog {
-
   recipeCtrl: FormControl;
   recipes:Recipe[] =[
-    {name: "Matar Paneer", time: 30},
-    {name: "Sambhar", time: 40},
-    {name: "Asperagus-Spaghetti", time: 60},
-    {name: "Noodles", time: 30},
-    {name: "MixDal", time: 30},
-    {name: "Alloo-Eggplant", time: 30},
-    {name: "Boda", time: 30},
-    {name: "Bread-Omelette", time: 30},
-    {name: "Sabu-Vada", time: 30}
+    {recipeName: "Matar Paneer", time: 30},
+    {recipeName: "Sambhar", time: 40},
+    {recipeName: "Asperagus-Spaghetti", time: 60},
+    {recipeName: "Noodles", time: 30},
+    {recipeName: "MixDal", time: 30},
+    {recipeName: "Alloo-Eggplant", time: 30},
+    {recipeName: "Boda", time: 30},
+    {recipeName: "Bread-Omelette", time: 30},
+    {recipeName: "Cereal", time: 5},
+    {recipeName: "Sabu-Vada", time: 30},
+    {recipeName: "Banana", time: 1}
  ]
 
  reactiveRecipes: any;
-
+ dialogResult : any;
 
   constructor(
     public dialogRef: MatDialogRef<AddDishDialog>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
 
-      this.recipeCtrl = new FormControl({name: 'Sambhar', time: 60});
+      this.recipeCtrl = new FormControl({recipeName: data.recipeName, time: data.time});
         this.reactiveRecipes = this.recipeCtrl.valueChanges
         .startWith(this.recipeCtrl.value)
         .map(val => this.displayFn(val))
-        .map(name => this.filterRecipes(name));
+        .map(recipeName => this.filterRecipes(recipeName));
      }
-
+  
+     onClick(mealType: string ): void {
+       this.dialogResult = {recipeName: this.recipeCtrl.value.recipeName, mealType: mealType }
+      this.dialogRef.close(this.dialogResult);
+    }
+  
   onNoClick(): void {
     this.dialogRef.close();
   }
 
+
   displayFn(value: any): string {
-    return value && typeof value === 'object' ? value.name : value;
+    return value && typeof value === 'object' ? value.recipeName : value;
   }
   filterRecipes(val: string) {
     return val ? this._filter(this.recipes, val) : this.recipes;
@@ -57,7 +59,6 @@ export class AddDishDialog {
 
   private _filter(recipes: Recipe[], val: string) {
     const filterValue = val.toLowerCase();
-    return recipes.filter(recipe => recipe.name.toLowerCase().startsWith(filterValue));
+    return recipes.filter(recipe => recipe.recipeName.toLowerCase().startsWith(filterValue));
   }
-
 }
--- src/modules/month/calendar-month-meal-view.component.ts
+++ src/modules/month/calendar-month-meal-view.component.ts
@@ -1,7 +1,8 @@
-import { Component,Inject,Input} from '@angular/core';
-import {MatDialog, MatDialogRef,MAT_DIALOG_DATA} from '@angular/material';
-import {AddDishDialog} from './add-dish-dialog';
+import { Component, Inject, Input, ChangeDetectorRef} from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { AddDishDialog } from './add-dish-dialog';
 import { MonthViewDay, MonthView } from 'calendar-utils';
+import { Recipe } from './Recipe';
 
 @Component({
   selector: 'cv-calendar-month-meal-view',
@@ -12,26 +13,66 @@ import { MonthViewDay, MonthView } from 'calendar-utils';
 export class CalendarMonthMealViewComponent {
 
   @Input() day: MonthViewDay;
-
-  recipeName: string ="Noodles";
-  recipeTime: string= "10mins";
   currentDay: MonthViewDay = this.day;
-  constructor(public dialog:MatDialog) { }
+  recipe: Recipe = new Recipe("Noodles", 30);
+  recipeB: Recipe = new Recipe("Cereal", 5);
+  recipeL: Recipe = new Recipe("Boda", 30);
+  recipeS: Recipe = new Recipe("Banana", 1);
+  recipeD: Recipe = new Recipe("Alloo-Eggplant", 30);
+   recipeObj : any;
+  constructor(public dialog: MatDialog, private cdRef: ChangeDetectorRef) { }
 
-  openDialog(): void {
+  
+  openDialog(MealType: string): void {
     console.log("My Date: " + this.day.date);
-    let mydateType = typeof(this.day.date);
-    console.log("My Date: " + mydateType);
+    let mydateType = typeof (this.day.date);
+    console.log(MealType);
+    switch (MealType) {
+      case "B":
+       this.recipeObj =  { recipeName: this.recipeB.recipeName, recipeTime: this.recipeB.time, currentDay: this.day, recipeType: "B" };
+       break;
+      case "L":
+      this.recipeObj =  { recipeName: this.recipeL.recipeName, recipeTime: this.recipeL.time, currentDay: this.day, recipeType: "L"  };
+      break;
+      case "S":
+      this.recipeObj =  { recipeName: this.recipeS.recipeName, recipeTime: this.recipeS.time, currentDay: this.day, recipeType: "S"  };
+      break;
+      case "D":
+      this.recipeObj =  { recipeName: this.recipeD.recipeName, recipeTime: this.recipeD.time, currentDay: this.day,recipeType: "D"  };
+       break;
+    }
+    console.log(this.recipeObj);
     let dialogRef = this.dialog.open(AddDishDialog, {
       width: '250px',
-      data: { recipeName: this.recipeName, recipeTime: this.recipeTime, currentDay: this.day }
+      data: this.recipeObj
     });
+
+
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      console.log(result.name);
-      this.recipeName = result.name;
-      console.log("This recipe name is " + this.recipeName);
+      console.log(result);
+      if(result != undefined){
+      //console.log(this.recipeObj);
+        switch (result.mealType) {
+        case "B":
+         this.recipeB =  { recipeName: result.recipeName, time: result.time};
+         break;
+        case "L":
+        this.recipeL =  { recipeName: result.recipeName, time: result.time};
+        break;
+        case "S":
+        this.recipeS =  { recipeName: result.recipeName, time: result.time};
+        break;
+        case "D":
+        this.recipeD =  { recipeName: result.recipeName, time: result.time};
+         break;
+      }
+    }
+      //this.recipeObj.recipeName = result.recipeName;
+      this.cdRef.markForCheck();
+      //console.log(this.recipeObj);
+      //console.log("This recipe name is " + this.recipe.recipeName);
     });
   }
 
